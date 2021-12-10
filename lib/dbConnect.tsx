fc732a274a1227755d02d5b224909024ebb30a9b
saurabhtopthon01 <55596640+saurabhtopthon01@users.noreply.github.com>
2021-12-10 22:00:09 +0530

    update shopping list

--- lib/dbConnect.tsx
+++ lib/dbConnect.tsx
@@ -1,56 +1,11 @@
 import mongoose from "mongoose";
 
-const connection = {};
 const MONGODB_URI = process.env.MONGODB_URI;
 
 async function dbConnect() {
-  if (connection.isConnected) {
-    return;
-  }
-
   const db = await mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-
-  connection.isConnected = db.connections[0].readyState;
 }
-
-// const MONGODB_URI = process.env.MONGODB_URI;
-
-// if (!MONGODB_URI) {
-//   throw new Error(
-//     "Please define the MONGODB_URI environment variable inside .env.local"
-//   );
-// }
-
-// /**
-//  * Global is used here to maintain a cached connection across hot reloads
-//  * in development. This prevents connections growing exponentially
-//  * during API Route usage.
-//  */
-// let cached = global.mongoose;
-
-// if (!cached) {
-//   cached = global.mongoose = { conn: null, promise: null };
-// }
-
-// async function dbConnect() {
-//   if (cached.conn) {
-//     return cached.conn;
-//   }
-
-//   if (!cached.promise) {
-//     const opts = {
-//       bufferCommands: false,
-//     };
-
-//     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-//       return mongoose;
-//     });
-//   }
-//   cached.conn = await cached.promise;
-//   return cached.conn;
-// }
-
 export default dbConnect;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,4 @@
 export { Header } from "./header";
-export { Main } from "./main";
-export { Button } from "./button";
-export { Cards } from "./cards";
-export { Footer } from "./footer";
 export { Wrapper } from "./wrapper";
 export { Shoppinglist } from "./Shoppinglist";
 export { ListForm } from "./form";
