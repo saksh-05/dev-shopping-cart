23c3d5b010edc7c6603eeab12d1fc72cf6046598
Saurabh Kumar singh <pcsingh050301@gmail.com>
2025-09-30 01:30:10 +0530

    Update next.config.js
    MongoDB cluster updated

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const withPlugins = require("next-compose-plugins");
 
 const nextConfiguration = {
   env: {
-    MONGODB_URI:
-      "mongodb+srv://imageUser:ImageUser@cluster0.nq078.mongodb.net/shoppingList?retryWrites=true&w=majority",
+    MONGODB_URI:"mongodb+srv://saksh_db_user:RJfBggiKHOmizPx6@cluster0.ycygdq9.mongodb.net/shoppingList?retryWrites=true&w=majority&appName=Cluster0",
+      // "mongodb+srv://imageUser:ImageUser@cluster0.nq078.mongodb.net/shoppingList?retryWrites=true&w=majority",
     DEV_URL: "http://localhost:3000/menu",
     PROD_URL: "",
   },
